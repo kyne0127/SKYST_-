deb8372e3f6fd85c26482d6173f18a35d873cf3a
sarcman8 <sarcman8@naver.com>
2024-03-24 00:55:47 +0900

    ALMOST

--- FASTAPI/main.py
+++ FASTAPI/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Form, Depends
+from fastapi import FastAPI, Form, Depends, HTTPException, Cookie, Response
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 from typing import List
@@ -6,16 +6,8 @@ from fastapi.staticfiles import StaticFiles
 from starlette.responses import HTMLResponse
 from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse, FileResponse
-from fastapi_sessions import (
-    SessionManager,
-    SessionSettings,
-)
-
-# 세션 설정
-session_settings = SessionSettings(secret_key="my_secret_key")
-# 세션 매니저 생성
-session_manager = SessionManager(session_settings)
-
+import uvicorn
+import json
 
 app = FastAPI()
 
@@ -25,10 +17,11 @@ users = []
 
 
 class Room(BaseModel):
-    name: str
+    owner: str
+    id: int
     users: list
-    capacity: int
-
+    usernum: int
+    flag: int
 
 class User(BaseModel):
     username: str
@@ -39,26 +32,118 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # 홈 페이지에 index.html 제공
 @app.get("/", response_class=HTMLResponse)
-async def index_response(session=Depends(session_manager)):
+async def index_response():
     return FileResponse("static/index.html")
 
 @app.post("/register/")
-def register_user(name: str = Form(...),session=Depends(session_manager)):
+def register_user(response:Response, name: str = Form(...)):
     # 받은 이름을 users 리스트에 추가
     if (name in users):
         return {"message": f"ALREADY have {name} "}
     else:
-        session["user_name"] = name
         users.append(name)
-        return RedirectResponse(url="/home")
+        cookie_value = str({'user_name':name})
+        response = RedirectResponse(url="/home")
+        response.set_cookie(key="example_cookie", value=cookie_value)
+        return response
     
 @app.post("/home/")
 def home():
     return FileResponse("static/home.html")
-async def create_room(room: Room):
+@app.post("/create-room/")
+async def create_room(room_id: int = Form(...), example_cookie: str = Cookie(None)):
+    for test_room in rooms:
+        if room_id == test_room.id:
+            return {"message: type other room"}
+    # 통과
+    print(room_id)
+    cookie_dict = eval(example_cookie)
+    room = Room(id=room_id, users=[cookie_dict['user_name']], usernum=1, owner=cookie_dict['user_name'], flag=0)
     rooms.append(room)
-    room.users.append(session)
-    return {"message": f"Room '{room.name}' created with capacity {room.capacity}"}
+    cookie_dict['room_id'] = room_id
+    cookie_value = str(cookie_dict)
+    print(cookie_value)
+    room_url = f"/room/{room_id}param1={room_id}"
+    response = RedirectResponse(url=room_url)
+    response.set_cookie(key="example_cookie", value=cookie_value)
+    print("here")
+    return response
+
+
+@app.post("/join-room/")
+async def join_room(join_id: int = Form(...), example_cookie: str = Cookie(None)):
+    i = 0
+    for test_room in rooms:
+        if join_id == test_room.id:
+            print("find room", join_id)
+            i = 1
+            break
+    if (i == 0):
+        return {"message: there is no room"}
+    #통과
+    cookie_dict = eval(example_cookie)
+    test_room.users.append(cookie_dict['user_name'])
+    test_room.usernum += 1
+    cookie_dict['room_id'] = join_id
+    cookie_value = str(cookie_dict)
+    room_url = f"/room/{join_id}param1={join_id}"
+    response = RedirectResponse(url=room_url)
+    response.set_cookie(key="example_cookie", value=cookie_value)
+    print("here")
+    return response
+
+
+@app.post("/room/{room_id}")
+def test():
+    return FileResponse("static/room.html")
+@app.get("/room/{room_id}")
+async def room_info(param1: int):
+    room_id = param1
+    print("HAHA")
+    print(room_id)
+    for test_room in rooms:
+        if room_id == test_room.id:
+            break
+    return {"room_id": room_id, "rooms": room_info}
+
+@app.post("/get-play")
+async def get_play(response:Response, example_cookie: str = Cookie(None)):
+    cookie_dict = eval(example_cookie)
+    print(cookie_dict)
+    room_id = cookie_dict['room_id']
+    user_name = cookie_dict['user_name']
+    i = 0
+    for test_room in rooms:
+        if room_id == test_room.id:
+            if test_room.owner == user_name:
+                #방장
+                test_room.flag = 1
+                cookie_dict['game_ctr'] = 0
+                response = RedirectResponse("/room/"+str(room_id)+"/game/"+str(cookie_dict['game_ctr']))
+                cookie_value = str(cookie_dict)
+                response.set_cookie(key="example_cookie", value=cookie_value)
+                return response
+            else:
+                #참여자
+                if test_room.flag == 0:
+                    #아직 시작 안함
+                    return RedirectResponse("/room/"+cookie_dict['room_id'])
+                else:
+                    #시작함.
+                    cookie_dict['game_ctr'] = 0
+                    response = RedirectResponse("/room/"+str(room_id)+"/game/"+str(cookie_dict['game_ctr']))
+                    cookie_value = str(cookie_dict)
+                    response.set_cookie(key="example_cookie", value=cookie_value)
+                    return response
+            
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+
 
 
 @app.post("/invite_user/{room_name}/")
@@ -85,7 +170,3 @@ async def get_users(room_name: str):
         raise HTTPException(status_code=404, detail="Room not found")
 
 
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="127.0.0.1", port=8000)
