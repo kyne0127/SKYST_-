be6b58a85ebe3efad11b08f1849d205af17b75eb
sarcman8 <sarcman8@naver.com>
2024-03-23 20:55:44 +0900

    this

--- FASTAPI/main.py
+++ FASTAPI/main.py
@@ -1,19 +1,32 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Form, Depends
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 from typing import List
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import HTMLResponse
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi_sessions import (
+    SessionManager,
+    SessionSettings,
+)
+
+# 세션 설정
+session_settings = SessionSettings(secret_key="my_secret_key")
+# 세션 매니저 생성
+session_manager = SessionManager(session_settings)
+
 
 app = FastAPI()
 
 # 임시적으로 저장할 데이터베이스
 rooms = []
-users = {}
+users = []
 
 
 class Room(BaseModel):
     name: str
-    us
+    users: list
     capacity: int
 
 
@@ -26,12 +39,25 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # 홈 페이지에 index.html 제공
 @app.get("/", response_class=HTMLResponse)
-async def get_home():
-    return open("static/index.html", "r").read()
+async def index_response(session=Depends(session_manager)):
+    return FileResponse("static/index.html")
 
-@app.post("/create_room/")
+@app.post("/register/")
+def register_user(name: str = Form(...),session=Depends(session_manager)):
+    # 받은 이름을 users 리스트에 추가
+    if (name in users):
+        return {"message": f"ALREADY have {name} "}
+    else:
+        session["user_name"] = name
+        users.append(name)
+        return RedirectResponse(url="/home")
+    
+@app.post("/home/")
+def home():
+    return FileResponse("static/home.html")
 async def create_room(room: Room):
     rooms.append(room)
+    room.users.append(session)
     return {"message": f"Room '{room.name}' created with capacity {room.capacity}"}
 
 
